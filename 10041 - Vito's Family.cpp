b2877c3ebb356a9a9ada31c8e2e1c0bed73b4726
Mahtab Hossain <63159321+MahtabTanim@users.noreply.github.com>
2020-07-02 01:12:31 +0600

    Create 10041 - Vito's Family.cpp

---
+++ 10041 - Vito's Family.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define MP ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long int
+#define maxn 1000000
+#define FP ff.open("output.txt")
+fstream ff;
+#define N 101
+ll a,b,c,d,k,h,e,f,t,m,n,i,j,x,y,count,tcase=0,l,r;
+string s1,s2;
+
+int main()
+{
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        vector<int> v(n);
+        for(int i = 0;i<n;i++){
+            cin>>v[i];
+        }
+        sort(v.begin(),v.end());
+        ll sum1= 0, a =v[n/2] ;
+        for(int i = 0;i<n;i++){
+            sum1 +=abs(v[i]-a);
+        }
+        cout<<sum1<<endl;
+    }
+    return 0;
+}
