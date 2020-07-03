493afa64c84a62aae9ae344c8f8034040f79cb61
Mahtab Hossain <63159321+MahtabTanim@users.noreply.github.com>
2020-07-03 21:52:46 +0600

    Create 10235 - Simply Emirp.cpp

---
+++ 10235 - Simply Emirp.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+//#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define MP ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long int
+#define maxn 1000000
+#define FP ff.open("output.txt")
+fstream ff;
+#define N 101
+ll a,b,c,d,k,h,e,f,t,m,n,i,j,x,y,count,tcase=0,l,r;
+string s1,s2,s;
+bool isprime(ll n)
+{
+    for(ll i = 2;i*i<=n;i++){
+        if((n%i)==0) return false;
+    }
+    return true;
+}
+int main()
+{
+    while(cin>>n)
+    {
+    	if(isprime(n))
+    	{
+    		b = n;
+    		ll sum = 0;
+    		while(n){
+    			sum = sum*10 + (n%10);
+    			n= n/10;
+    		}
+    		if(b!=sum && isprime(sum)) cout<<b<<" is emirp.\n";
+    		else cout<<b<<" is prime.\n";
+    	}
+    	else cout<<n<<" is not prime.\n";
+    }
+    return 0;
+}
