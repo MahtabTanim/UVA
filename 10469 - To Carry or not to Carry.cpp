bb7c38cc79a5e860c8b0638ac294b27c05ab2ea4
Mahtab Hossain <63159321+MahtabTanim@users.noreply.github.com>
2020-07-17 19:44:41 +0600

    Create 10469 - To Carry or not to Carry.cpp

---
+++ 10469 - To Carry or not to Carry.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define MP ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long int
+#define N 1378
+#define maxn 1000000
+#define FP ff.open("output.txt")
+#define FR ff.open("output.txt")
+fstream ff;
+
+ll a,q,b,c,d,k,h,e,f,t,m,n,i,j,x,y,count,tcase=0,l,r;
+string s1,s2,s;
+
+int main()
+{   
+    while(cin>>a>>b)
+    {
+      cout<<(a^b)<<endl;
+    }
+    return 0;
+}
