69b2e032c0e345d04a618d18b93cd1ff2eae55da
Mahtab Hossain <63159321+MahtabTanim@users.noreply.github.com>
2020-07-25 19:45:34 +0600

    Create 673 - Parentheses Balance.cpp

---
+++ 673 - Parentheses Balance.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+using namespace std;#
+define mod 1000000007# define pb push_back# define mp make_pair# define MP ios_base::sync_with_stdio(false);
+cin.tie(0);
+cout.tie(0);#
+define IG cin.ignore(numeric_limits < streamsize > ::max(), '\n');#
+define ll long long int# define N 1378# define maxn 1000003# define FP ff.open("output.txt")# define FR ff.open("output.txt")
+fstream ff;
+
+ll a, q, b, c, d, k, h, e, f, t, m, n, i, j, x, y, count, tcase = 0, l, r;
+string s1, s2, s;
+
+bool isbalanced(string s) {
+  stack < char > st;
+  for (int i = 0; i < s.size(); i++) {
+    if (s[i] == '[' || s[i] == '(') {
+      st.push(s[i]);
+    } else {
+      if (st.empty()) return false;
+      if (s[i] == ')') {
+        if (st.top() != '(') return false;
+        st.pop();
+      } else {
+        if (st.top() != '[') return false;
+        st.pop();
+      }
+    }
+  }
+  if (!(st.empty())) return false;
+  return true;
+}
+
+int main() {
+  FP;
+  cin >> t;
+  getchar();
+  while (t--) {
+    getline(cin, s);
+    if (isbalanced(s) || s == "\n") cout << "Yes\n";
+    else cout << "No\n";
+  }
+  return 0;
+}
