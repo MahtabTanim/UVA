22ab7c60df00dc0362adcb6b93c243741d7d802b
Mahtab Hossain <63159321+MahtabTanim@users.noreply.github.com>
2020-07-29 01:54:51 +0600

    Create 12149 - Feynman.cpp

---
+++ 12149 - Feynman.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define MP ios_base::sync_with_stdio(false);
+cin.tie(0);
+cout.tie(0);
+#define IG cin.ignore(numeric_limits < streamsize > ::max(), '\n');
+#define ll long long int
+#define N 1378
+#define maxn 1000003
+#define FP ff.open("output.txt")
+#define FR ff.open("output.txt")
+#define valid(a, b) if (a >= 1 && a < row - 1 && b >= 1 && b < col - 1)
+    fstream ff;
+
+ll a, q, b, c, d, k, h, e, f, t, m, n, i, j, x, y, count, tcase = 0, l, r;
+string s1, s2, s;
+
+int main() {
+    FP;
+    while (cin >> n && n) {
+        a = n * (n + 1) * (2 * n + 1);
+        cout << a / 6 << endl;
+    }
+    return 0;
+}
