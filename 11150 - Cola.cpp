dc186acb1280c12dd90cb1dec58f8cb7da8042eb
Mahtab Hossain <63159321+MahtabTanim@users.noreply.github.com>
2020-08-04 21:29:45 +0600

    Create 11150 - Cola.cpp

---
+++ 11150 - Cola.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define MP ios_base::sync_with_stdio(false);
+cin.tie(0);
+cout.tie(0);
+#define IG cin.ignore(numeric_limits < streamsize > ::max(), '\n');
+#define ll long long int
+#define N 1378
+#define maxn 1000003
+#define FP ff.open("output.txt")
+#define FR ff.open("output.txt")
+#define valid(a, b) if (a >= 1 && a < row - 1 && b >= 1 && b < col - 1)
+    fstream ff;
+
+ll a, q, b, c, d, k, h, e, f, t, m, n, i, j, x, y, count, tcase = 0, l, r;
+string s1, s2, s;
+
+int main() {
+    while (cin >> n) {
+        ll r = 0;
+        r = n + n / 3;
+        n = n / 3 + n % 3;
+        while (n >= 3) {
+            r += (n / 3);
+            n = n / 3 + n % 3;
+        }
+        if (n == 2) r++;
+        cout << r << endl;
+    }
+    return 0;
+}
